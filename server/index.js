c53afefc76c0510e2988297bfbe151c5f7b7179a
yasser massoud <yassermassoud000@gmail.com>
2020-10-16 21:56:25 +0100

    CRUD DONE

--- server/index.js
+++ server/index.js
@@ -1,22 +1,20 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose')
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const app = express();
-const Movies = require("../database-mongo")
+const Movies = require("../database-mongo");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-// app.use('/static', express.static(__dirname + 'react-client/dist'));
-app.use(express.static(__dirname + '/../react-client/dist'));
 
-// Routers 
-const movieRouter = require('../routers/movieRouter');
-const user = require('../routers/usersRouter');
+app.use(express.static(__dirname + "/../react-client/dist"));
 
 
-app.use('/movie', movieRouter)
-app.use('/user', user)
+const movieRouter = require("../routers/movieRouter");
+const user = require("../routers/usersRouter");
+
+app.use("/movie", movieRouter);
+app.use("/user", user);
 
 app.listen(3000, () => {
-  console.log('listening on port 3000!');
+  console.log("listening on port 3000!");
 });
-
